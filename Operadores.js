afb6c7617768b34cc144f332321d889764bfd098
Jonathan <https://gitlab.com/jonathaneduardo>
2023-04-11 18:16:29 -0300

    incremento e decremento

--- Operadores.js
+++ Operadores.js
@@ -1,6 +1,12 @@
-5 + 2 
-5 / 2
-5 - 2
-5 * 2
-5 % 2
-5 ** 2
+5 + 2  //Adição//
+5 / 2 //Divisão//
+5 - 2 //Subtração//
+5 * 2 //Multiplicação//
+5 % 2 //Resto Divisão//
+5 ** 2 //Potênciação//
+
+var n = 3
+n = n + 4
+var num = 8
+num += 2
+num
