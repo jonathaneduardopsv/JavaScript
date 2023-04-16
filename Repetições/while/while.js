86f57f827cd7347d15449b13fc55e4eaeb93de5d
Jonathan <https://gitlab.com/jonathaneduardo>
2023-04-16 11:58:00 -0300

    repetições while e do while

---
+++ Repetições/while/while.js
@@ -0,0 +1,11 @@
+//estrutura de repetição com teste logico no inicio
+var c = 1
+while (c <= 3){
+    console.log(`passo ${c}`)
+    c++
+}
+//estrutura de repetição com teste logico no final
+do{
+    onsole.log(`passo ${c}`)
+    c++
+}while(c <= 3)
