a4ca25d5fefe4e87f55b794167ade98be7da7f3c
Jonathan <https://github.com/jonathaneduardopsv>
2023-04-18 21:02:02 -0300

    Get

---
+++ Date/get.js
@@ -0,0 +1,15 @@
+const dataAtual = new Date();
+
+console.log( dataAtual.getUTCFullYear() );
+console.log( dataAtual.getUTCMonth() );
+console.log( dataAtual.getUTCDate() );
+console.log( dataAtual.getUTCHours() );
+console.log( dataAtual.getUTCMinutes() );
+console.log( dataAtual.getUTCSeconds() );
+console.log( dataAtual.getUTCMilliseconds() );
+console.log( dataAtual.getUTCDay() );
+
+
+console.log( dataAtual.getTime() );
+console.log( dataAtual.getTimezoneOffset() );
+console.log( dataAtual.valueOf() );
--- Date/timestamp.js
+++ Date/timestamp.js
@@ -15,4 +15,4 @@ const horas = minutos / 60;
 const dia = horas / 24
 
 // Exibindo a diferença
-console.log(dia);
+console.log(`${dia} o ano atual é ${dataPostagem.getFullYear()}`);
